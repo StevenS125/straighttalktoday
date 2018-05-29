ef573fe15ced5d1dc4389a500f5b8b0134c86fa0
StevenS125 <sts12589@gmail.com>
2018-05-29 09:28:13 -0400

    changed garbage collector so app doesnt crash all the time

--- src/Advert/Advert.js
+++ src/Advert/Advert.js
@@ -26,9 +26,16 @@ class Advert extends Component {
 
         <div className={styles.advertcontainer}>
             <h3>What We Offer</h3>
-            <AdPane title="Motivational Speaking" content="From conception and strategy to design and implementation, Piranha Technologies builds and hosts premium websites for businesses, associations, government, and Fortune 500 companies" icon="fas fa-chart-line fa-7x"></AdPane>
-            <AdPane title="Life Coaching" content="Piranha Technologies' integrations team has the experience and expertise to seamlessly integrate any third party software that your organization utilizes in order to give your users the best online experience possible." icon="fas fa-clipboard fa-7x"></AdPane>
-            <AdPane title="Services training and development" content="With the influx of smartphones, tablets, and everything in between, it's quickly become mandatory that your business is accessible on the go via a mobile site or app." icon="fas fa-shipping-fast fa-7x"></AdPane>
+            <AdPane title="Motivational Speaking" content=" - embracing your handicap
+                                                            - reaching your full potential 
+                                                            - discovering who you are
+                                                            - identifying your strengths and weaknesses " icon="fas fa-chart-line fa-7x"></AdPane>
+            <AdPane title="Life Coaching 1 on 1" content=" -Taking control of your finances
+- becoming in tune with your abilities 
+-knowing your worth " icon="fas fa-clipboard fa-7x"></AdPane>
+            <AdPane title="Services training and development" content=" -Real estate investment 
+-planning for your future 
+-money management " icon="fas fa-shipping-fast fa-7x"></AdPane>
         </div>
 </div>
     );

48c32db38a533032226ad05212f1fadd69459b29
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2018-06-07 18:31:57 -0400

    updated services styling

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Table } from 'reactstrap';
 
 //components
 
@@ -15,7 +16,7 @@ class Events extends Component {
     return (
         <div className={styles.eMain}>
         <h4 className={styles.eHead}>Upcoming Events</h4>
-        <table className="table table-hover">
+        <Table hover>
         <thead>
           <tr>
             <th scope="col">Event</th>
@@ -30,7 +31,7 @@ class Events extends Component {
             <td>Catherine & Count Basie Middle School 72,<br></br> 133-25 Guy R. Brewer Blvd., Rm. 209, 1 Jamaica, Ny 11434</td>
           </tr>
         </tbody>
-      </table>
+      </Table>
         </div>
     );
   }

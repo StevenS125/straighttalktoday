d2c21744e688c72c79802915078ead9d7434d35a
StevenS125 <sts12589@gmail.com>
2018-10-08 16:07:23 -0400

    new stuff

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -17,21 +17,33 @@ class Events extends Component {
       event: []
     };
       
-      // this.createTable = this.createTable.bind(this);
-      console.log(this.state)
-     
+      this.createTable = this.createTable.bind(this);
+         
     
       }
      
 
-      componentDidMount(){
-        
-        fetch("https://sttapi.herokuapp.com/events/?format=json").then((res) => res.json())
-            .then((myjson) => this.setState({event: JSON.stringify(myjson)}) 
-            )
-          console.log(this.state)
-          }
-         
+   componentWillMount() {
+     this.getEvents()
+   }
+
+   componentDidMount() {
+  setTimeout(() => {
+    console.log(this.state.event[0][0].name);
+  }, 4000);
+  
+  }
+
+
+getEvents() {
+          fetch("https://sttapi.herokuapp.com/events/?format=json")
+          .then((res) => res.json())
+            .then((myjson) => {this.setState({
+              event: [myjson]
+            });
+            
+          } )
+}
 
         
 
@@ -49,24 +61,23 @@ class Events extends Component {
 //   this.createTable()
 // }
 
- createTable = () => {
-    let table = []
-    // Outer loop to create parent
-    for (let i = 0; i < 2; i++) {
-      let children = [];
-      let children2 = [];
-      let children3 = []
-      //Inner loop to create children
-        children.push(<td>{`${this.state.event[i].name}`}</td>);
-        children2.push(<td>{`${this.state.event[i].logdate}`}</td>);
-        children3.push(<td>{`${this.state.event[i].address}`}</td>);
-        table.push(<tr>{children}{children2}{children3}</tr>)
-      }
-      //Create the parent and add the children
-      
+createTable = () => {
+  let table = []
+  // Outer loop to create parent
+  for (let i = 0; i < 2; i++) {
+    let children = [];
+    let children2 = [];
+    let children3 = []
+    //Inner loop to create children
+      children.push(<td>{`${this.state.event[0][i].name}`}</td>);
+      children2.push(<td>{`${this.state.event[0][i].logdate}`}</td>);
+      children3.push(<td>{`${this.state.event[0][i].address}`}</td>);
+      table.push(<tr>{children}{children2}{children3}</tr>)
+    }
     
-    return table
-  }
+  
+  return table
+}
 
 
 
@@ -93,7 +104,6 @@ class Events extends Component {
         </thead>
         <tbody>
           
-       
           <tr>
             <td>Youth Summit</td>
             <td>October 6th 8:30AM-12:30PM</td>
